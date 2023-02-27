651d984f44b2f6171fb7d30f9feab708eee3545a
git\Sugarshock <sephiger@hotmail.com>
2023-02-27 18:37:22 -0300

    escritura de los cv final

---
+++ js/function.js
@@ -0,0 +1,10 @@
+var btn1 = document.getElementById("boton-diploma1");
+var btn2 = document.getElementById("boton-diploma2");
+
+btn1.addEventListener("click", function() {
+ window.open("https://i.ibb.co/MG2bbzS/ARQUEOLOGO.jpg");
+});
+  
+btn2.addEventListener("click", function() {
+ window.open("https://i.ibb.co/RTyCkSD/ARQUEOLOGO-MARINO.jpg");
+});
